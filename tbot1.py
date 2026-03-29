17568c2399290225d4b23241f0955e1dc8b589ff
omer79 <omerelkhooly79@gmail.com>
2026-03-29 14:45:37 +0300

    Create tbot1.py

---
+++ tbot1.py
@@ -0,0 +1,13 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+TOKEN = "8646597177:AAEwMn82f00-S6KqxSjO-P07sokuLAUwSDM"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Hello from your bot")
+
+app = ApplicationBuilder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()
