aae252b325e83811ed593d44d2a3d466921c15aa
omer79 <omerelkhooly79@gmail.com>
2026-03-29 15:35:19 +0300

    Update tbot1.py

--- tbot1.py
+++ tbot1.py
@@ -4,7 +4,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 TOKEN = "8646597177:AAEwMn82f00-S6KqxSjO-P07sokuLAUwSDM"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Hello from your bot")
+    await update.message.reply_text("Hello from your bot helloooo")
 
 app = ApplicationBuilder().token(TOKEN).build()
 
